5c1f2d8719cf39e683623f1ce22d782c263eb1e0
dbyoung720 <dbyoung@sina.com>
2023-03-15 07:36:18 +0800

    add module dbVideoSDK

---
+++ modules/dbVideo/vsdk.dpr
@@ -0,0 +1,20 @@
+library vsdk;
+{$IF CompilerVersion >= 21.0}
+{$WEAKLINKRTTI ON}
+{$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+{$IFEND}
+
+uses
+  System.SysUtils,
+  System.Classes,
+  uCommon in '..\uCommon.pas',
+  db.VideoSDK in 'db.VideoSDK.pas',
+  Unit1 in 'Unit1.pas' {frmVideo};
+
+{$R *.res}
+
+exports
+  db_ShowDllForm_Plugins;
+
+begin
+end.
